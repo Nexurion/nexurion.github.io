9cee5e6dc91a9b60ee4e49b90f6950a6587b50b1
Alexander "Nexurion" Drochnev <acunit26@gmail.com>
2022-11-20 14:50:09 +0300

    Add files via upload

--- py_scripts/crazy_computer.py
+++ py_scripts/crazy_computer.py
@@ -60,5 +60,4 @@ else:
     print("Выключение компьютера через: ")
     time.sleep(1)
     print("3..."), time.sleep(1), print("2..."), time.sleep(1), print("1..."), time.sleep(1)
-#    print("BOO!!!")
     os.system("shutdown /r /t 0")
